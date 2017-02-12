401ebb6f779a15ebc41ab95f2e7f7954c5ef9061
Mark Vayngrib <mark@tradle.io>
2017-02-12 05:34:31 -0500

    initial comit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const co = require('bluebird').coroutine
+const Promise = require('bluebird')
+const co = Promise.coroutine
 const METHODS = [
   'get',
   'put',
   'del',
   'clear',
   'list',
-  'batch'
+  'batch',
+  'close'
 ]
 
 exports.wrap = function wrap ({ store }) {
@@ -13,15 +15,59 @@ exports.wrap = function wrap ({ store }) {
     assert(typeof store[method] === 'function', `expected store to have method "${method}"`)
   })
 
+  let closed
+  let closing
+
   const wrapped = {}
   METHODS.forEach(method => {
     // promisify
     wrapped[method] = co(function* (...args) {
+      validate(method, args)
       return store[method](...args)
     })
   })
 
   return wrapped
+
+  function validate (method, args) {
+    if (method === 'close') {
+      if (closing) throw new Error('already closing')
+
+      closing = true
+      return
+    }
+
+    if (method === 'put') {
+      validateKey(args[0])
+      validateValue(args[1])
+      return
+    }
+
+    if (method === 'batch') {
+      assert(args.length === 1, 'expected 1 argument')
+      args[0].forEach(op => {
+        const { type, key, value } = op
+        assert(type === 'put' || type === 'del', 'expected "type" to be "put" or "del"')
+        validateKey(key)
+        if (op === 'put') {
+          validateValue(value)
+        }
+      })
+    }
+  }
 }
 
-exports.default = require('./lowdb')
+function validateKey (key) {
+  assert(typeof key === 'string', 'key should be a string')
+}
+
+function validateValue (value) {
+  assert(value !== null && value !== undefined, 'value cannot be null or undefined')
+  assert(typeof value === 'string' || typeof value === 'object', 'value should be string or a plain javascript object')
+}
+
+function assert (statement, err) {
+  if (!statement) {
+    throw new Error(err || 'assertion failed')
+  }
+}
--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 
+const path = require('path')
+const rimraf = require('rimraf')
 const test = require('tape')
 const Promise = require('bluebird')
 const co = Promise.coroutine
 const { wrap } = require('./')
+const testDBPath = path.resolve(__dirname, './teststore')
 
-module.exports = function (implementation) {
-  const store = wrap(implementation)
-  const test = require('tape')
+module.exports = function testStore ({ create }) {
+  cleanup()
   test('put, get, list, delete', co(function* (t) {
+    let store = newStore(create)
     const bill = 'bill'
     const billInfo = { lastName: 'preston' }
     yield store.put(bill, billInfo)
@@ -31,5 +34,54 @@ module.exports = function (implementation) {
 
     yield store.clear()
     t.same(yield store.list(), [])
+
+    yield store.close()
+    try {
+      yield store.get(bill)
+      t.fail('store should be closed')
+    } catch (err) {
+    }
+
+    // close, reopen
+    store = newStore(create)
+    t.same(yield store.list(), [])
+    yield store.close()
+
+    t.end()
+    cleanup()
   }))
+
+  test('batch', co(function* (t) {
+    let store = newStore(create)
+    const a = { some: 'object' }
+    yield store.put('a', a)
+    const batch = [
+      { type: 'del', key: 'a' },
+      { type: 'put', key: 'b', value: '2' },
+      { type: 'put', key: 'c', value: '3' },
+    ]
+
+    yield store.batch(batch)
+    const results = yield store.list()
+    t.same(results, [
+      { key: 'b', value: '2' },
+      { key: 'c', value: '3' },
+    ])
+
+    yield store.close()
+    t.end()
+    cleanup()
+  }))
+}
+
+function cleanup () {
+  rimraf.sync(testDBPath)
+}
+
+function newStore (create) {
+  return wrap({
+    store: create({
+      path: testDBPath
+    })
+  })
 }
